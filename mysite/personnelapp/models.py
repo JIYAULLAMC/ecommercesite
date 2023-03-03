c63b9d716117daf98129bbc15cb039971654d617
JIYACHINDADI <jiyachindadi@gmail.com>
2023-03-03 23:57:51 +0530

    added the employee model in personnell table and linked with the user built in model

--- mysite/personnelapp/models.py
+++ mysite/personnelapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Employee model
+
+class Employee(models.Model):
+    user = models.ForeignKey(User)
+    emp_code = models.IntegerField( unique=True, null=False)
+    emp_fname = models.CharField(null=False)
+    emp_lname = models.CharField()
+    emp_sal = models.FloatField()
+    emp_phone = models.PositiveBigIntegerField()
+    emp_email = models.EmailField(max_length=30)
+    emp_address = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return f'{self.emp_code}_{self.emp_fname}'
+
